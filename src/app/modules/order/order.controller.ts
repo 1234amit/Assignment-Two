1dfbd822860f7f222fd49439b82ebcd0ea5ddac3
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2024-05-23 16:21:23 +0600

    updated

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -3,36 +3,6 @@ import { orderSchema } from "./order.validator";
 import { OrderServices } from "./order.service";
 import { ProductServices } from "../product/product.service";
 
-// export const createOrder = async (req: Request, res: Response) => {
-//   try {
-//     const { error } = orderSchema.validate(req.body);
-//     if (error) {
-//       return res
-//         .status(400)
-//         .json({ success: false, message: error.details[0].message });
-//     }
-
-//     const order = await OrderServices.createOrderIntoDb(req.body);
-//     res.status(201).json({
-//       success: true,
-//       message: "Order created successfully!",
-//       data: order,
-//     });
-//   } catch (err) {
-//     if (err instanceof Error) {
-//       if (
-//         err.message === "Product not found" ||
-//         err.message === "Insufficient quantity available in inventory"
-//       ) {
-//         return res.status(400).json({ success: false, message: err.message });
-//       }
-//       res.status(500).json({ success: false, message: "Server error" });
-//     } else {
-//       res.status(500).json({ success: false, message: "Unknown server error" });
-//     }
-//   }
-// };
-
 export const createOrder = async (req: Request, res: Response) => {
   try {
     const { error } = orderSchema.validate(req.body);
