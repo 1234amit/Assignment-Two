8722e3a3b4de89e56b5aeeef9ae0874ef82cad27
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2024-05-23 15:21:55 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-// import mongoose from "mongoose";
-// import app from "./app";
-// import config from "./app/config";
-
 // async function main() {
 //   try {
 //     await mongoose.connect(config.database_url as string);
